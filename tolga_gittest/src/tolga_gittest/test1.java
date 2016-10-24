06ef86c323e19eceeaf54720c9bff874cfb2a704
tolgae <tolgae@Tolgae-ED.tani.local>
2016-10-24 15:33:10 +0300

    degisiklik2

--- tolga_gittest/src/tolga_gittest/test1.java
+++ tolga_gittest/src/tolga_gittest/test1.java
@@ -6,6 +6,8 @@ public class test1 {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("hello world2");
+		System.out.println("hello world3")
+		
 
 	}
 
